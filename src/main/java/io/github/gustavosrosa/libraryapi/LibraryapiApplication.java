ea60073669bf935f9f899bd1b940c6eefbeea6c3
Gustavo Rosa <g.dsantosrosa@gmail.com>
2026-01-06 10:07:24 -0300

    FIX: Removing spring banner

--- src/main/java/io/github/gustavosrosa/libraryapi/LibraryapiApplication.java
+++ src/main/java/io/github/gustavosrosa/libraryapi/LibraryapiApplication.java
@@ -1,13 +1,18 @@
 package io.github.gustavosrosa.libraryapi;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 
 @SpringBootApplication
 public class LibraryapiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibraryapiApplication.class, args);
+		SpringApplicationBuilder springApplicationBuilder = new SpringApplicationBuilder(LibraryapiApplication.class);
+		
+		springApplicationBuilder.bannerMode(Mode.OFF);
+		springApplicationBuilder.run(args);
 	}
 
 }
